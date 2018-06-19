ff8301aa249377f3ed07316168fcbb12e2b06089
Edgar Ordóñez <edrodriguez2310@gmail.com>
2018-06-19 17:44:17 +0200

    changed entry point

--- App.js
+++ App.js
@@ -1,39 +1,7 @@
 // @flow
 
-import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { Font } from 'expo';
+import BitHubLab from './app/bithublab';
 
-import { BitHubLabLogo } from './app/components';
+const APP = BitHubLab;
 
-export default class App extends React.Component {
-  state = {
-    fontLoaded: false,
-  };
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      'Roboto-Regular': require('./app/assets/fonts/Roboto-Regular.ttf'),
-      'Roboto-Black': require('./app/assets/fonts/Roboto-Black.ttf'),
-    });
-
-    this.setState({ fontLoaded: true });
-  }
-
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        {this.state.fontLoaded ? <BitHubLabLogo /> : undefined}
-      </SafeAreaView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FAFAFA',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default APP;
