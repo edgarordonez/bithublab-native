a556ece679b11f80d2db122d3dae7a60a418e17c
Edgar Ordóñez <edrodriguez2310@gmail.com>
2018-08-26 17:53:54 +0200

    Added mocks and __test__ folder

---
+++ app/mocks/repositories.js
@@ -0,0 +1,23 @@
+// @flow
+import { Repository } from '@bithublab/components';
+
+export const REPOSITORES: Repository[] = [
+  {
+    provider: 'github',
+    name: 'D3-Stencil',
+    description: 'Components of graphs build with StencilJS & D3.js',
+    url: 'https://www.npmjs.com/package/d3-stencil',
+  },
+  {
+    provider: 'gitlab',
+    name: 'D3-Stencil',
+    description: 'Components of graphs build with StencilJS & D3.js',
+    url: 'https://www.npmjs.com/package/d3-stencil',
+  },
+  {
+    provider: 'bitbucket',
+    name: 'D3-Stencil',
+    description: 'Components of graphs build with StencilJS & D3.js',
+    url: 'https://www.npmjs.com/package/d3-stencil',
+  }
+];
